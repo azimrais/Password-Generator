4d8186ab2528c1ffe76a1ead92df7964be51c6a2
azimrais <138938610+azimrais@users.noreply.github.com>
2023-08-13 12:56:25 +0530

    Add files via upload

---
+++ Password Generator/script.js
@@ -0,0 +1,30 @@
+class password {
+    constructor() {
+        this.pass = ""
+    }
+    generatePassword(len) {
+        let chars = "QWERTYUIOPASDFGHJKLZXCVBNM"
+        let char = "qwertyuiopasdfghjklzxcvbnm"
+        let numbers = "7894561230"
+        let special = "!@#$%^&*~?"
+
+        let i = 0;
+        while (i < len) {
+            this.pass += chars[Math.floor(Math.random() * chars.length)]
+            this.pass += char[Math.floor(Math.random() * char.length)]
+            this.pass += numbers[Math.floor(Math.random() * numbers.length)]
+            this.pass += special[Math.floor(Math.random() * special.length)]
+            i+=4
+        }
+        return this.pass
+    }
+}
+let p = new password()
+let inp = prompt("Enter the length of the password you waht to generate")
+let m = p.generatePassword(inp)
+if (inp < 3) {
+    cont.innerHTML = "Yor password should be atleast 3 charactes long"
+}
+else {
+    cont.innerHTML = m
+}
